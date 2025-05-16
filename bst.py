62d145f52af1fb4aebccd9444869274fa89e698e
Dalilah_H <dalilahlovehicks@gmail.com>
2025-05-16 15:26:08 -0700

    starting

--- bst.py
+++ bst.py
@@ -4,8 +4,13 @@ from typing import *
 from dataclasses import dataclass
 sys.setrecursionlimit(10**6)
 
+BinTree : TypeAlias = Union[None, 'BSNode']
 @dataclass(frozen=True)
-class BST:
+class BSNode:
+    val: Any
+    left: BinTree
+    right: BinTree
+
 
 def example_fun(x : int) -> bool:
     return x < 142
