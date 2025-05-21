c591ebbc3b94765a1755e11f55cb18de08fa03bb
EzliCruz <soccaezli05@gmail.com>
2025-05-21 00:39:05 -0700

    did the lookup and delete functions

--- bst.py
+++ bst.py
@@ -31,7 +31,64 @@ class BinarySearchTree:
                         return BSNode(t.val, insert_helper(l, val), r)
                     else :
                         return BSNode(v, l, insert_helper(r, val))
-# Not done
+
+    # Not done
+
+    def lookup(self, t: 'BinarySearchTree', val: Any) -> bool:
+        def lookup_helper(t: BinTree):
+            match t:
+                case None:
+                    return False
+                case BSNode(v, l , r):
+                    if self.comes_before(v, val) is False and self.comes_before(val, v) is False:
+                        return True
+                    elif self.comes_before(val, v):
+                        return lookup_helper(l)
+                    else:
+                        return lookup_helper(r)
+        return lookup_helper(t.other)
+
+    def delete(self, t1:'BinarySearchTree', val1: Any) -> BinTree:
+        def val_check(val, com_val):
+            return self.comes_before(val, com_val) is False and self.comes_before(com_val, val) is False
+
+        def left_min(t: BinTree):
+            match t:
+                case BSNode(v, None, r):
+                    return v
+                case BSNode(v, l, r):
+                    return left_min(l)
+
+        def delete_helper(t: BinTree):
+            match t:
+                case None:
+                    return None
+                case BSNode(v, None, None):
+                    if val_check(v, val1):
+                        return None
+                    else:
+                        return t
+                case BSNode(v, None, r):
+                    if val_check(v, val1):
+                        return r
+                    else:
+                        return BSNode(v, None, delete_helper(r))
+                case BSNode(v, l, None):
+                    if val_check(v, val1):
+                        return l
+                    else:
+                        return BSNode(v, delete_helper(l), None)
+                case BSNode(v, l, r):
+                    if val_check(v, val1):
+                        most_min = left_min(r)
+                        return BSNode(most_min, l, delete_helper(r))
+                    elif t1.comes_before(val1, v):
+                        return BSNode(v, delete_helper(l), r)
+                    else:
+                        return BSNode(v,l,delete_helper(r))
+
+        return delete_helper(t1.other)
+
 
 
 
