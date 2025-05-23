6cbc78c51cd6ee2a07d05a72fe7905248c394a84
Dalilah_H <dalilahlovehicks@gmail.com>
2025-05-23 12:11:00 -0700

    starting

--- bst.py
+++ bst.py
@@ -2,54 +2,35 @@ import sys
 import unittest
 from typing import *
 from dataclasses import dataclass
-sys.setrecursionlimit(10**6)
 
-BinTree : TypeAlias = Union[None, 'BSNode']
+sys.setrecursionlimit(10 ** 6)
+
+BinTree: TypeAlias = Union[None, 'BSNode']
+
+
 @dataclass(frozen=True)
 class BSNode:
     val: Any
     left: BinTree
     right: BinTree
 
+
 @dataclass(frozen=True)
 class BinarySearchTree:
-    comes_before: Callable[[Any, Any],bool]
+    comes_before: Callable[[Any, Any], bool]
     other: BinTree
-    def is_empty(self)-> bool:
-        if self.other is None:
-            return True
-        else:
-            return False
 
-    def insert(self, t : 'BinarySearchTree', val: Any)-> 'BinarySearchTree':
-        def insert_helper(t: BinTree, val: Any)->BinTree:
-            match t:
-                case None:
-                    return BSNode(val, None, None)
-                case BSNode(v, l, r):
-                    if self.comes_before(val,v):
-                        return BSNode(v, insert_helper(l, val), r)
-                    else:
-                        return BSNode(v, l, insert_helper(r, val))
-        tree = insert_helper(t.other,val)
+    def is_empty(self) -> bool:
+        return self.other is None
+
+    def insert(self, t: 'BinarySearchTree', val: Any) -> 'BinarySearchTree':
+        tree = insert_helper(t.other, val, self.comes_before)
         return BinarySearchTree(self.comes_before, tree)
 
-
     def lookup(self, t: 'BinarySearchTree', val: Any) -> bool:
-        def lookup_helper(t: BinTree):
-            match t:
-                case None:
-                    return False
-                case BSNode(v, l , r):
-                    if self.comes_before(v, val) is False and self.comes_before(val, v) is False:
-                        return True
-                    elif self.comes_before(val, v):
-                        return lookup_helper(l)
-                    else:
-                        return lookup_helper(r)
-        return lookup_helper(t.other)
+        return lookup_helper(t.other, val, self.comes_before)
 
-    def delete(self, t:'BinarySearchTree', val1: Any) -> BinTree:
+    def delete(self, t: 'BinarySearchTree', val1: Any) -> BinTree:
         def val_check(val, com_val):
             return self.comes_before(val, com_val) is False and self.comes_before(com_val, val) is False
 
@@ -85,17 +66,34 @@ class BinarySearchTree:
                     elif self.comes_before(val1, v):
                         return BSNode(v, delete_helper(l), r)
                     else:
-                        return BSNode(v,l,delete_helper(r))
+                        return BSNode(v, l, delete_helper(r))
 
         return delete_helper(t.other)
 
 
+def insert_helper(t: BinTree, val: Any, comes_before: Callable[[Any, Any], bool]) -> BinTree:
+    match t:
+        case None:
+            return BSNode(val, None, None)
+        case BSNode(v, l, r):
+            if comes_before(val, v):
+                return BSNode(v, insert_helper(l, val, comes_before), r)
+            else:
+                return BSNode(v, l, insert_helper(r, val, comes_before))
 
 
+def lookup_helper(t: BinTree, val: Any, comes_before: Callable[[Any, Any], bool]):
+    match t:
+        case None:
+            return False
+        case BSNode(v, l, r):
+            if comes_before(v, val) is False and comes_before(val, v) is False:
+                return True
+            elif comes_before(val, v):
+                return lookup_helper(l, val, comes_before)
+            else:
+                return lookup_helper(r, val, comes_before)
 
 
-
-
-
-def example_fun(x : int) -> bool:
+def example_fun(x: int) -> bool:
     return x < 142
--- bst_tests.py
+++ bst_tests.py
@@ -2,17 +2,59 @@ import sys
 import unittest
 from typing import *
 from dataclasses import dataclass
-sys.setrecursionlimit(10**6)
+import math
+
+import bst
+
+sys.setrecursionlimit(10 ** 6)
 
 from bst import *
- bst =  BinarySearchTree()
+@dataclass(frozen=True)
+class Point:
+    x: int
+    y: int
+
+def comes_before_num(a: int, b: int):
+    return a < b
+
+
+def comes_before_abc(a: str, b: str):
+    return a < b
+
+
+def comes_before_euclidean(a: Point, b: Point):
+    dist_a = ((a.x ** 2) +(a.y ** 2)) ** 0.5
+    dist_b = ((b.x ** 2) +(b.y ** 2)) ** 0.5
+    return dist_a < dist_b
+
+bt1 = BSNode(8, BSNode(3, None, None), BSNode(10, None, None))
+bt2 = BSNode("E", BSNode("D", None, None), BSNode("G", None, None))
+
+bst1 = BinarySearchTree(comes_before_num, bt1)
+bst2 = BinarySearchTree(comes_before_abc, bt2)
+bst3 = BinarySearchTree(comes_before_num, None)
+
 class BSTTests(unittest.TestCase):
     def test_is_empty(self):
+        self.assertEqual(True, bst3.is_empty())
+        self.assertEqual(False, bst1.is_empty())
 
+    def test_insert_helper(self):
+        expected = BSNode(8, BSNode(3, BSNode(2, None, None), None), BSNode(10, None, None))
+        expected2 = BSNode("E", BSNode("D", BSNode("A", None, None), None), BSNode("G", None, None))
+        self.assertEqual(expected, insert_helper(bt1, 2, comes_before_num))
+        self.assertEqual(expected2, insert_helper(bt2, "A", comes_before_abc))
+
+    def test_insert(self):
+        pass
+
+    def test_look_up_helper(self):
+        pass
 
     def test_example_fun(self):
         self.assertEqual(True, example_fun(34))
-        self.assertEqual(False,example_fun(1423))
+        self.assertEqual(False, example_fun(1423))
+
 
 if (__name__ == '__main__'):
     unittest.main()
