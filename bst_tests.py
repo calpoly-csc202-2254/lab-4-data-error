e773a13c9ed87674045aadc3bf9f11b0bd04b590
Dalilah_H <dalilahlovehicks@gmail.com>
2025-05-23 12:24:50 -0700

    working

--- bst_tests.py
+++ bst_tests.py
@@ -9,10 +9,13 @@ import bst
 sys.setrecursionlimit(10 ** 6)
 
 from bst import *
+
+
 @dataclass(frozen=True)
 class Point:
     x: int
     y: int
+
 
 def comes_before_num(a: int, b: int):
     return a < b
@@ -23,9 +26,10 @@ def comes_before_abc(a: str, b: str):
 
 
 def comes_before_euclidean(a: Point, b: Point):
-    dist_a = ((a.x ** 2) +(a.y ** 2)) ** 0.5
-    dist_b = ((b.x ** 2) +(b.y ** 2)) ** 0.5
+    dist_a = ((a.x ** 2) + (a.y ** 2)) ** 0.5
+    dist_b = ((b.x ** 2) + (b.y ** 2)) ** 0.5
     return dist_a < dist_b
+
 
 bt1 = BSNode(8, BSNode(3, None, None), BSNode(10, None, None))
 bt2 = BSNode("E", BSNode("D", None, None), BSNode("G", None, None))
@@ -33,6 +37,7 @@ bt2 = BSNode("E", BSNode("D", None, None), BSNode("G", None, None))
 bst1 = BinarySearchTree(comes_before_num, bt1)
 bst2 = BinarySearchTree(comes_before_abc, bt2)
 bst3 = BinarySearchTree(comes_before_num, None)
+
 
 class BSTTests(unittest.TestCase):
     def test_is_empty(self):
@@ -44,6 +49,7 @@ class BSTTests(unittest.TestCase):
         expected2 = BSNode("E", BSNode("D", BSNode("A", None, None), None), BSNode("G", None, None))
         self.assertEqual(expected, insert_helper(bt1, 2, comes_before_num))
         self.assertEqual(expected2, insert_helper(bt2, "A", comes_before_abc))
+        # needs euclidean test case
 
     def test_insert(self):
         pass
@@ -51,11 +57,11 @@ class BSTTests(unittest.TestCase):
     def test_look_up_helper(self):
         self.assertEqual(False, lookup_helper(bt1, 2, comes_before_num))
         self.assertEqual(True, lookup_helper(bt2, "E", comes_before_abc))
-
+      #   needs euclidean test case
 
 def test_example_fun(self):
-        self.assertEqual(True, example_fun(34))
-        self.assertEqual(False, example_fun(1423))
+    self.assertEqual(True, example_fun(34))
+    self.assertEqual(False, example_fun(1423))
 
 
 if (__name__ == '__main__'):
